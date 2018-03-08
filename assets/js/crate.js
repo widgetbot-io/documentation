6a14d29a1a0c58ef55f715ad320cdc2d98a972f3
samdd <sam@samdd.me>
2018-03-08 10:02:16 +0000

    Deployed 4daf40e with MkDocs version: 0.17.2

--- assets/js/crate.js
+++ assets/js/crate.js
@@ -5,4 +5,7 @@ script.setAttribute('defer', true)
 
 script.innerHTML = 'new Crate({server:"299881420891881473",colors:{toggle:"#7E57C2"},channel:"368427726358446110",options:"0102",delay:!0,logo:"widgetbot",beta:!0});'
 
+window.addEventListener('crate', function() {
+  document.body.classList.add('crate-loaded')
+})
 document.head.appendChild(script)
